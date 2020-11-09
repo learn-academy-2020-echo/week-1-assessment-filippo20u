1ce3d4e4f9f6fae05c001067ed40f075e508016a
filippo20u <filippo.20u@gmail.com>
2020-11-08 19:53:17 -0800

    Week 1 ass., did my best. try to finish on Mon morning

--- testing-with-jest.test.js
+++ testing-with-jest.test.js
@@ -9,12 +9,37 @@
 // --------------------1a) Create a test for a function called addThemUp that takes two numbers as arguments and returns the sum.
 
 
+describe("addThemUpNo", () => {
+    test( "add two numbers" , () => {
+      expect(addThemUp()).toEqual(16)
+        })
+})
+
+var numb1 = 10
+var numb2 = 5
+
+const addThemUpNo =  () => {
+    { return numb1 + numb2} 
+}
 
 
 
 // --------------------1b) See the test fail. THEN write the code to make the test pass.
 
+//previous code just have a wrong return to make sure the function is working
 
+describe("addThemUp", () => {
+    test( "add two numbers" , () => {
+      expect(addThemUp()).toEqual(15)
+        })
+})
+
+var numb1 = 10
+var numb2 = 5
+
+const addThemUp =  () => {
+    { return numb1 + numb2} 
+}
 
 
 
@@ -22,6 +47,35 @@
 
 
 
+describe("triAreaNo", () => {
+    test ("calculate a triangle area", () => {
+      expect(triAreaNo()).toEqual(50)
+    })        
+      
+})
+
+//it wil fail, the var babNo has the wrong value
+
+var babNo = 100
+var height = 10
+
+const triAreaNo = () => {
+    { return ((babNo * height)/2)} 
+}
 
 
 // --------------------2b) See the test fail. THEN write the code to make the test pass.
+
+describe("triArea", () => {
+    test ("calculate a triangle area", () => {
+      expect(triArea()).toEqual(50)
+    })        
+      
+})
+
+var bab = 10
+var height = 10
+
+const triArea = () => {
+    { return ((bab * height)/2)} 
+}
